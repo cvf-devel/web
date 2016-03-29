4a004f5c4698e794981f1746f5c0a642e4a48241
zachjanicki <zjanicki88@gmail.com>
2016-03-28 20:25:25 -0400

    Automated git commit, Mon Mar 28 20:25:25 EDT 2016

--- web_backup.py
+++ web_backup.py
@@ -36,12 +36,12 @@ os.system(sys_call)
 for file_name in file_stream:
 	# create a copy of the file with a .bak extension
 	sys_call = "cp " + str(file_name) + " " + str(file_name) + ".bak"
-	print sys_call
+	print repr(sys_call)
 	# os.system(sys_call)
 	# mv the .bak to the 
 	file_bak = str(file_name) + ".bak"
-	print sys_call
 	#sys_call = "mv " + file_bak + "Dropbox/" + file_bak
+	print repr(sys_call)
 	print file_bak + " moved"
 
 
