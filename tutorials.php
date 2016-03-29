6175f4309633c8ea060eefedde82fe51c3acc686
zachjanicki <zjanicki88@gmail.com>
2016-03-28 20:18:02 -0400

    Automated git commit, Mon Mar 28 20:18:02 EDT 2016

---
+++ tutorials.php
@@ -0,0 +1,13 @@
+<?php
+     $which_page = "tutorials";
+     include('common_header.php');
+?>
+
+
+<table>
+<tr><td colspan="3" align="center"><h2>CVPR 2014 Accepted Tutorials</h2><br/>Organizers, please contact the <a href="http://www.google.com/recaptcha/mailhide/d?k=01VJaxQuH5TSFlMs9ukFD_cA==&c=GP8iwEfxR72664efAY4EDVwv7oXZ6WJL0M45Zn2UXtz2G6m_gUofETEcJroKO5Su" target="_blank">Website Chairs</a> with needed updates. </td></tr>
+<tr><td colspan="3">
+<table cellspacing="0" cellpadding="2" width="100%">
+<tr><td width="15%" align="center"><u><b>Date</b></u></td><td width="*"><u><b>Tutorial Title / Organizers</b></u></td></tr>
+
+
