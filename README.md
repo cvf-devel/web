7078e0af4ff2cf1bf691dc3237618327947b5513
zachjanicki <zjanicki88@gmail.com>
2016-04-12 21:14:36 -0400

    updated README.md with backup restoration instructions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 
 Websites and content for the Computer Vision Foundation
+
+To restore the backup for the conference websites up to 2015, run ./restore_backup.sh in the conferences directory.
+It will say that ./web and /home/cvfconf/conferences/web are the same directory but it still runs mv just fine.
+
+
--- remove_old_backups.py
+++ remove_old_backups.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python2.7
 
 import os
+import sys
 
+path_to_dropbox = sys.argv[1]
 stream = os.popen("find ~/Dropbox -name '*.gz'")
 list_of_backups = []
 for item in stream:
 	list_of_backups.append(item)
 	
 if len(list_of_backups) > 4:
+	os.chdir(path_to_dropbox)
+	print os.getcwd()
 	stream = os.popen("ls -lS")
 	# delete the first entry b/c its the oldest
 	stream.readline() # to get rid of the first output which is useless
 	old_entry = stream.readline()
 	old_entry = old_entry.strip("\n")
+	old_entry = old_entry.split(' ')
+	# its in old_entry[9]
 	print repr(old_entry)
-	sys_call = "rm " + str(old_entry)
+	sys_call = "rm " + str(old_entry[9])
 	os.system(sys_call)
